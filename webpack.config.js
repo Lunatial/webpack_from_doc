fb49efb5a4fa4f40ab3d35e24b2b93194a78566f
Feleky Mirkó <feleky@gmail.com>
2019-02-11 11:02:43 +0100

    end of development section

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     app: "./src/index.js",
     print: "./src/print.js"
   },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true
+  },
+  devtool: "inline-source-map",
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(["dist"]),
     new HtmlWebpackPlugin({
       title: "Output Management"
     })
