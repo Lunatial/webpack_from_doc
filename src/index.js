3786dd8836307bf2563c10c90b762a389f3dd136
Feleky Mirkó <feleky@gmail.com>
2019-02-11 11:23:53 +0100

    end of hot module replacement

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
-import _ from 'lodash';
-import './css/style.css';
-import printMe from './print.js';
+import _ from "lodash";
+import "./css/style.css";
+import printMe from "./print.js";
 
-var arr = ['kutya', 'macska', 'sün'];
+var arr = ["kutya", "macska", "sün"];
 
-function component(arr = ['Hello', 'from', 'webpack']) {
-    let element = document.createElement('div');
+function component(arr = ["Hello", "from", "webpack"]) {
+  let element = document.createElement("div");
 
-    element.innerHTML = _.join(arr, ' ');
-    element.classList.add('hello');
+  element.innerHTML = _.join(arr, " ");
+  element.classList.add("hello");
 
-    var btn = document.createElement('button');
+  var btn = document.createElement("button");
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
 
-    element.appendChild(btn);
-    return element;
-  }
+  element.appendChild(btn);
+  return element;
+}
 
-  document.body.appendChild(component(arr));
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept("./print.js", function() {
+    console.log("Accepting the updated printMe module!");
+    document.body.removeChild(element);
+    element = component(); // Re-render the "component" to update the click handler
+    document.body.appendChild(element);
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "development",
   entry: {
-    app: "./src/index.js",
-    print: "./src/print.js"
+    app: "./src/index.js"
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
-    compress: true
+    compress: true,
+    hot: true
   },
   devtool: "inline-source-map",
   plugins: [
     new CleanWebpackPlugin(["dist"]),
     new HtmlWebpackPlugin({
       title: "Output Management"
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: "[name].bundle.js",
