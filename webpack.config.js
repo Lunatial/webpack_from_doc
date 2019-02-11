52de42aef627ae598aa679d5a2e3c6d18e9fdc45
Feleky Mirkó <feleky@gmail.com>
2019-02-11 14:45:25 +0100

    code refactor

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
   entry: {
     app: path.join(__dirname, "/src/index.js")
   },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
@@ -30,10 +34,6 @@ module.exports = {
       chunkFilename: mode !== "production" ? "[id].css" : "[id].[hash].css"
     })
   ],
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
   module: {
     rules: [
       {
@@ -44,17 +44,6 @@ module.exports = {
           "postcss-loader"
         ]
       },
-      //   {
-      //     test: /\.m?js$/,
-      //     exclude: /(node_modules|bower_components)/,
-      //     use: {
-      //       loader: "babel-loader",
-      //       options: {
-      //         presets: ["@babel/preset-env"],
-      //         plugins: ["transform-es2015-modules-commonjs"]
-      //       }
-      //     }
-      //   },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
@@ -74,6 +63,17 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ["file-loader"]
       }
+      //   {
+      //     test: /\.m?js$/,
+      //     exclude: /(node_modules|bower_components)/,
+      //     use: {
+      //       loader: "babel-loader",
+      //       options: {
+      //         presets: ["@babel/preset-env"],
+      //         plugins: ["transform-es2015-modules-commonjs"]
+      //       }
+      //     }
+      //   },
     ]
   }
 };
