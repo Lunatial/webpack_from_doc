13a6d072e926dc900665084b7de9c5d9bf0f0756
Feleky Mirkó <feleky@gmail.com>
2019-02-11 10:17:04 +0100

    end of asset management

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+import './css/style.css';
+import Icon from './img/usj_gG3b.jpg';
 
 var arr = ['kutya', 'macska', 'sün'];
 
@@ -6,6 +8,13 @@ function component(arr = ['Hello', 'from', 'webpack']) {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(arr, ' ');
+    element.classList.add('hello');
+
+    // Add the image to our existing div.
+    // var myIcon = new Image();
+    // myIcon.src = Icon;
+
+    // element.appendChild(myIcon);
 
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,36 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      }
+    ]
   }
 };
