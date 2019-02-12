ad78faef6769b7d00e6a548100cc01a81a707d99
Feleky Mirkó <feleky@gmail.com>
2019-02-12 11:27:22 +0100

    dynamic imports try

--- src/index.js
+++ src/index.js
@@ -40,3 +40,16 @@ if (module.hot) {
     document.body.appendChild(element);
   });
 }
+
+ async function getComponent() {
+   var element = document.createElement('div');
+     const { join } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+
+       element.innerHTML = join(['Hello', 'webpack'], ' ');
+
+       return element;
+  }
+
+getComponent().then(component => {
+    document.body.appendChild(component);
+});
